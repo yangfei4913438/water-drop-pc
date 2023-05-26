656524f975c9e949d6d80e7e9ee01fb6fd2f7f3c
杨飞 <yangfei86@vip.qq.com>
2023-05-26 16:28:58 +0800

    集成jotai

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,8 @@ import path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import eslint from 'vite-plugin-eslint';
+import jotaiDebugLabel from 'jotai/babel/plugin-debug-label';
+import jotaiReactRefresh from 'jotai/babel/plugin-react-refresh';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,7 +15,7 @@ export default defineConfig({
     proxy: {},
     port: 5001,
   },
-  plugins: [react(), eslint()],
+  plugins: [react({ babel: { plugins: [jotaiDebugLabel, jotaiReactRefresh] } }), eslint()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'), // 映射的目录必须以/开头，表示根目录
